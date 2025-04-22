5c66f07d3a8b5a0dd9aa00863224684bc98f8adc
vik-4994 <igor.gavrylenko05@gmail.com>
2025-04-22 18:07:49 +0200

    Added all application

---
+++ src/main/java/org/example/lab2web/Lab2WebApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2WebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab2web/Lab2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
